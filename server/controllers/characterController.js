1c10268ddebe534364c0f07ab8cabe6bb591103d
Graham Albachten <albachteng@gmail.com>
2021-05-03 22:27:54 -0500

    more middleware logic

--- server/controllers/characterController.js
+++ server/controllers/characterController.js
@@ -1,12 +1,13 @@
 const db = require('../models/characterModels');
 
-// object will export all character controller functions for our API router
+// object will export all character controller middleware functions for our API router
 const characterController = {};
 
 // createCharacter requests for making a new Character model and saving to the database
-
+characterController.getCharacter = () => {};
 // getCharacter for displaying saved characters
-
+characterController.getCharacter = () => {};
 // updateCharacter (PUT) for changes to characters being rendered
-
+characterController.getCharacter = () => {};
 // deleteCharacter for permanently deleting characters from the database
+characterController.deleteCharacter = () => {};
