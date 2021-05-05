b4a3635e65b95c8abd05f806c8adc1411548ca58
Graham Albachten <albachteng@gmail.com>
2021-05-05 11:54:24 -0500

    switch to mongoose

--- server/controllers/characterController.js
+++ server/controllers/characterController.js
@@ -5,17 +5,41 @@ const characterController = {};
 
 // createCharacter requests for making a new Character model and saving to the database
 characterController.createCharacter = (req, res, next) => {
-
-};
+    const queryText = 
+        `INSERT INTO characters 
+        (...columns)
+        VALUES (...$1, $2, $3...)`;
+    const queryParams = []; // TODO pull params off of req.body?
+    db.query(queryText, queryParams)
+        .then(queryResponse => {
+            res.locals.addCharacter = queryResponse.rows;
+            next();
+        })
+        .catch((err) => {
+            console.log(err);
+            next({log: 'Express error handler caught at createCharacter'});
+        })
+    };
 
 // getCharacter for displaying saved characters
 characterController.getCharacter = (req, res, next) => {
-    
+    const queryParams = [req.query.id]
+    const queryText = 
+        `SELECT * FROM characters 
+        WHERE _id='$1'`;
+    db.query(queryText, queryParams)
+        .then(queryResponse => {
+            res.locals.character = queryResponse.rows;
+        })
+        .catch((err) => {
+            console.log(err);
+            next({log: 'Express error handler caught at getCharacter'})
+        })
 };
 
 // updateCharacter (PUT) for changes to characters being rendered
 characterController.updateCharacter = (req, res, next) => {
-
+    
 };
 
 // deleteCharacter for permanently deleting characters from the database
