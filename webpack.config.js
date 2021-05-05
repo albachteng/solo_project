2276d31315c2b835957bdbbea66409134ad22914
Graham Albachten <albachteng@gmail.com>
2021-05-04 21:02:56 -0500

    update webpack.config for hot updates and install pg for postgres work

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path'); 
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // doubt i will need this... 
+// const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin'); // ditto
 
 module.exports = {
     entry: './client/index.js',
@@ -6,19 +8,30 @@ module.exports = {
         path: path.resolve(__dirname, 'build'), 
         filename: 'bundle.js',
     },
-    mode: 'development',
+    mode: process.env.NODE_ENV,
     module: {
-        rules: [{
+        rules: [
+            {
             test: /\.jsx?/,
             use: {
                 loader: 'babel-loader', 
                 options: {
                     presets: ['@babel/preset-env', '@babel/preset-react'],
+                    
                     // plugins: [
                     //     '@babel/plugin-transform-runtime',
                     // ]
-                }
-            }
-        }]
-    }
+                }}
+            },
+            {
+            test: /\s[ac]ss$/i,
+            use: ['style-loader', 'css-loader', 'sass-loader',]
+            },
+        ]  
+    },
+    devServer: {
+        publicPath: '/build',
+        // proxy: {},
+        hot: true,
+    },
 };
